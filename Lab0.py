9090b1c970ce30e29940cfd49c441117ba4522fb
ccnazar <ccnazar@loyola.edu>
2024-09-11 15:43:31 -0400

    Testing

--- Lab0.py
+++ Lab0.py
@@ -1,4 +1,4 @@
-#Programmers:
+#Programmers: Cooper Nazar and Jalen Henderson
 #Course: CS151, Dr. Yalew
 #Due Date: 9/12/24
 #Lab Assignment: 0
@@ -10,15 +10,15 @@
 print("CS151 Lab 00")
 
 print("Partner 1: Cooper")
-print("Fact 1: ")
-print("Fact 2: ")
-print("Fact 3: ")
-print("Fact 4: ")
-print("Fact 5: ")
+print("Fact 1: Has two dogs")
+print("Fact 2: Likes to read")
+print("Fact 3: Likes libraries")
+print("Fact 4: Has been to England")
+print("Fact 5: Lived in Connecticut all his life")
 
-print("Partner 2: ")
-print("Fact 1: ")
-print("Fact 2: ")
-print("Fact 3: ")
-print("Fact 4: ")
-print("Fact 5: ")
+print("Partner 2: Jalen")
+print("Fact 1: Ran track and field")
+print("Fact 2: Attended Loyola Blakefield High School")
+print("Fact 3: Took a gap year after his freshman year of college")
+print("Fact 4: Is currently a manager at Chick-Fil-A")
+print("Fact 5: His Aunt was president of Loyola Maryland's BSU")
